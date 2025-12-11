ecd755e2de6ed30825788509c9a55e6546e3e444
RodrigoSY <sihues3@gmail.com>
2025-12-11 06:00:53 -0500

    Nueva IP

--- sanisidro-frontend/src/services/api.js
+++ sanisidro-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const API_URL = 'https://backend-production-cbbe.up.railway.app/api';
 // Para pruebas locales
 // const API_URL = 'http://localhost:8080/api';
-const API_URL = 'http://104.197.34.63:8080/api'
+const API_URL = 'http://35.202.39.203:8080/api'
 
 // Configuración de axios
 const api = axios.create({
